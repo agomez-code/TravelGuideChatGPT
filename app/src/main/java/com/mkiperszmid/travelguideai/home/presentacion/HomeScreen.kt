7718985dd871d086e2a52dee65d592502eff3ea8
agomez-code <agomez.code@gmail.com>
2024-02-22 17:52:01 -0300

    scroll content

--- app/src/main/java/com/mkiperszmid/travelguideai/home/presentacion/HomeScreen.kt
+++ app/src/main/java/com/mkiperszmid/travelguideai/home/presentacion/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -33,28 +34,35 @@ fun HomeScreen(
         )
     }
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        Text(text = "A donde viajas?")
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(30.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-            ) {
-            HomeSearchBar(
-                onSearch = {
-                    viewModel.search()
-                },
-                placeholder = "Pais, CIudad",
-                inputText = state.searchText,
-                onValueChange = { viewModel.onSearchTextChange(it) }
-            )
-            HomeFilterButton(onClick = { viewModel.onFilterClick() })
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        item {
+            Text(text = "A donde viajas?")
         }
 
-        state.chatReply?.let {
-            Text(text = it)
+        item {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(30.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                HomeSearchBar(
+                    onSearch = {
+                        viewModel.search()
+                    },
+                    placeholder = "Pais, CIudad",
+                    inputText = state.searchText,
+                    onValueChange = { viewModel.onSearchTextChange(it) }
+                )
+                HomeFilterButton(onClick = { viewModel.onFilterClick() })
+            }
+        }
+
+        item {
+            state.chatReply?.let {
+                Text(text = it)
+            }
         }
 
     }
